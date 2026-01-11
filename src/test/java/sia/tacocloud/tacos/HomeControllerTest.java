270f72468ade3209f2448ec80ac33485e6477ece
evgenivanov1980 <ivanov.evgen.v@gmail.com>
2026-01-11 14:18:14 +0500

    Replace HomeController to WebConfig.java

--- src/test/java/sia/tacocloud/tacos/HomeControllerTest.java
+++ src/test/java/sia/tacocloud/tacos/HomeControllerTest.java
@@ -4,13 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(HomeController.class)
+@WebMvcTest()
 class HomeControllerTest {
 
     @Autowired
